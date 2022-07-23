556ea6b1231421c9aeab27212c22c5a091ebaa9b
Jenia Sakirko <Jenia.Sakirko@cyberark.com>
2022-07-23 22:18:10 +0300

    Add register to base user

--- backend/base_user/views.py
+++ backend/base_user/views.py
@@ -1,3 +1,20 @@
-# from django.shortcuts import render
-#
-# # Create your views here.
+from base_user.models import BaseUser
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import BaseUserSerializer
+
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = BaseUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        base_user = serializer.save()
+        _, token = AuthToken.objects.create(base_user.user)
+        return Response({
+            "user": BaseUserSerializer(base_user,
+                                       context=self.get_serializer_context()).data,
+            "token": token
+        })
