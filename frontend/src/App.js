83d1e3d45c02a983bb43fea43c7ebab64a1fed1c
Jenia Sakirko <Jenia.Sakirko@cyberark.com>
2022-07-29 15:10:03 +0300

    Add landing page and 404 page
    Signed-off-by: Jenia Sakirko <Jenia.Sakirko@cyberark.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import PageNotFound from './components/pages/PageNotFound'
+import LandingPage from './components/pages/LandingPage';
+
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                {/*<Navbar/>*/}
+                <Routes>
+                    <Route path='/' element={<LandingPage/>} exact/>
+
+                    {/*<Route path='/login' element={<Login/>} exact/>*/}
+                    {/*<Route path='/signup' element={<Signup/>} exact/>*/}
+                    {/*<Route path='/logout' element={<Logout/>} exact/>*/}
+                    <Route path='*' element={<PageNotFound/>} exact/>
+
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
